78761d7247e9f5adffbca63365a2601be9b2daf1
Kumar Ankush <000.kumarankush@gmail.com>
2022-04-17 15:48:06 +0530

    minor bugs fixed

--- src/unsplash.js
+++ src/unsplash.js
@@ -1,14 +1,12 @@
-// import { API_KEY } from "./secrets"
-
-const BASE_URL = `https://api.unsplash.com`
-const COUNT = 30
-const API_KEY = process.env.REACT_APP_API_KEY
+const BASE_URL = `https://api.unsplash.com`;
+const COUNT = 30;
+const API_KEY = process.env.REACT_APP_API_KEY;
 export function getRandomImages() {
-    const URL = `${BASE_URL}/photos/random/?client_id=${API_KEY}&count=${COUNT}`
-    return fetch(URL)
+  const URL = `${BASE_URL}/photos/random/?client_id=${API_KEY}&count=${COUNT}`;
+  return fetch(URL);
 }
 
 export function searchImages(searchTerm) {
-    const URL = `${BASE_URL}/search/photos/?client_id=${API_KEY}&per_page=${COUNT}&query=${searchTerm}`
-    return fetch(URL)
+  const URL = `${BASE_URL}/search/photos/?client_id=${API_KEY}&per_page=${COUNT}&query=${searchTerm}`;
+  return fetch(URL);
 }
